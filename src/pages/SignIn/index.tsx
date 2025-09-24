5f01503e8eb9d5fe1b664ecba41f2c9b7fa5775b
KarolSalvador <karolsalvador91@gmail.com>
2025-09-24 15:23:38 -0300

    feat: Adiciona estrutura básica da página de login e estado local

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,3 +1,44 @@
+import React, { useState } from "react";
+
 export default function SignIn() {
-  return <h1>Página de Login</h1>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("Email", email);
+    console.log("Password", password);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="email">E-mail</label>
+          <input
+            type="email"
+            id="email"
+            placeholder="@gmail.com"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            placeholder="*************"
+            value={password}
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+              setPassword(event.target.value)
+            }
+          />
+        </div>
+        <div>
+          <button type="submit">Sign In</button>
+        </div>
+      </form>
+    </div>
+  );
 }
