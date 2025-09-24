9ca88375aa37810cdf1680f5d751c2590577979e
KarolSalvador <karolsalvador91@gmail.com>
2025-09-24 14:37:40 -0300

    feat: Adiciona as rotas para login e perfil em App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import SignIn from "./pages/SignIn";
+import Profile from "./pages/Profile";
 
 function App() {
-  return <div>texto aqui</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<SignIn />} />
+      <Route path="/profile" element={<Profile />} />
+    </Routes>
+  );
 }
 
 export default App;
